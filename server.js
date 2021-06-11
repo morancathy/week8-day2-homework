4fd522fa438bfee0561b8aceda6384120d4cc97f
Cat <morancathy@gmail.com>
2021-06-10 22:17:42 -0500

    Fibonacci

--- server.js
+++ server.js
@@ -17,14 +17,31 @@ app.get('/magic/:question', (req, res) => {
   res.send(`<h1>Your question: ${req.params.question}?</h1><h1>Your answer: ${responseArray()}.</h1>`)
 })
 
-//Functions
+//Fibonacci
+app.get('/fibonacci/:num', (req, res) => {
+  isFibonacci(req.params.num)? res.send(`${req.params.num} is a Fibonacci Number`):
+                    res.send(`${req.params.num} is NOT a Fibonacci Number`);
+})
+
+//Functions ############################################
+//Magic 8 Ball Function
 const responseArray = () => {
   let quotes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
   let response = quotes[Math.floor(Math.random() * quotes.length)];
   return response;
 }
-
-
+//Fibonacci Function ...is num fib set-up?
+const isFibonacci = (num) =>
+{
+    return isPerfectSquare(5 * num * num + 4) || isPerfectSquare(5 * num * num - 4);
+}
+//Fibonacci Function ...finding perfect square
+const isPerfectSquare = (num) =>
+{
+    let sRoot = parseInt(Math.sqrt(num));
+    return (sRoot * sRoot == num);
+}
+//###################################################
 
 //listening PORT
 app.listen(PORT, () => {
